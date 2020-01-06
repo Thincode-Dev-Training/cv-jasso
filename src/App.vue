9aa3175d8e5bc9fd3cc6116cd12a62e3d278372e
Jorge Jasso <jorge.jasso@thincode.com>
2020-01-06 16:14:54 -0600

    Add styles to the page

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,20 @@
 <template>
   <v-app>
-    <div>
+    <v-container id="container" class="grey lighten-2">
       <v-row>
-        <v-col cols="3" class="mt-12">
-          <v-card class="mx-auto" max-width="300">
-            <v-img
-              src="../src/assets/images.jpg"
-              aspect-ratio="1"
-              class="grey lighten-2"
-              max-width="500"
-              max-height="500">
-            </v-img>
-            <v-list>
+        <v-col cols="3">
+          <br>
+          <v-card class="mt-12">
+            <v-img src="../src/assets/image.png" aspect-ratio="1" class="teal lighten-4"></v-img>
+            <v-list class="teal darken-4">
               <v-list-item-group>
                 <v-list-item v-for="(item, i) in items" :key="i">
                   <v-list-item-icon>
-                    <v-icon v-text="item.icon"></v-icon>
+                    <v-icon v-text="item.icon" class="white--text"></v-icon>
                   </v-list-item-icon>
-                  <router-link :to="item.route" id="routerlink" class="black--text">
+                  <router-link id="routerlink" :to="item.route">
                     <v-list-item-content :to="item.route">
-                      <v-list-item-title v-text="item.text"></v-list-item-title>
+                      <v-list-item-title v-text="item.text" class="white--text"></v-list-item-title>
                     </v-list-item-content>
                   </router-link>
                 </v-list-item>
@@ -28,47 +23,37 @@
           </v-card>
         </v-col>
         <v-col cols="9">
-          <v-content>
-            <router-view/>
-          </v-content>
+          <router-view id="routerview" class="teal lighten-4"/>
         </v-col>
       </v-row>
-    </div>
+    </v-container>
   </v-app>
 </template>
 
 <script>
   export default {
     name: 'App',
-    components: {
-
-    },
     data: () => ({
       items: [
-        {
-          route: '/',
-          text: 'Home',
-          icon: 'mdi-home',
-        },
-        {
-          route: '/about',
-          text: 'About',
-          icon: 'mdi-account-circle',
-        },
-        {
-          route: '/resume',
-          text: 'Resume',
-          icon: 'mdi-account-card-details',
-        },
-      ],
-    }),
+        { route: '/', text: 'Home', icon: 'mdi-home' },
+        { route: '/about', text: 'About me', icon: 'mdi-account-circle' },
+        { route: '/resume', text: 'Resume', icon: 'mdi-account-card-details' }
+      ]
+    })
   };
 </script>
 
 <style scoped>
+  #container{
+    box-shadow: 5px 0px 10px black, -5px 0px 10px black;
+  }
   #routerlink{
     display: inline-block;
     width: 300px;
     text-decoration: none;
+  }
+  #routerview{
+    height: 88%;
+    margin-top:10px;
   }
 </style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,81 +1,65 @@
 <template>
   <v-app>
-    <div class="about">
-      <v-row>
-        <v-col class="mt-12">
-          <v-card class="mx-auto mt-5 px-5" max-width="99%">
-            <p>I am Web Developer.</p>
-            <p>Full Stack developer, mainly focussed on Web applications, but still experienced with desktop apps. Passionate about learning, teaching, creating software and music.</p>
-          </v-card>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col cols="5">
-          <v-card max-width="350" tile>
-            <h1 class="text-center">Contact Info</h1>
-            <v-list-item v-for="(contact, i) in contacts" :key="i">
-              <v-list-item-content>
-                <v-list-item-title>
-                  <v-list-item-icon>
-                    <v-icon v-text="contact.icon"></v-icon>
-                    <span class="ml-3"> {{contact.text}} </span>
-                  </v-list-item-icon>
-                </v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
-          </v-card>
-        </v-col>
-        <v-col cols="7">
-          <v-card max-width="525" tile>
-            <h1 class="text-center">Diplomas</h1>
-            <v-list-item v-for="(diploma, i) in diplomas" :key="i">
-              <v-list-item-content>
-                <v-list-item-title>
-                  <v-list-item-icon>
-                    <v-icon v-text="diploma.icon"></v-icon>
-                    <span class="ml-3"> {{diploma.text}} </span>
-                  </v-list-item-icon>
-                </v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
-          </v-card>
-        </v-col>
-      </v-row>
-    </div>
+    <v-row>
+      <v-col class="mt-8">
+        <v-card class="mx-4 py-1 px-1 grey lighten-2">
+          <h1 class="mb-3 teal darken-4 white--text text-center">About me</h1>
+          <p class="text-center"><strong>I'm a Developer.</strong></p>
+          <p>I am a responsible and persevering developer who likes to learn and use technologies focused on both web development and desktop applications.</p>
+          <p>I am passionate about learning, creating and improving my skills to develop software with high quality.</p>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row style="margin-top: -70px;">
+      <v-col cols="5">
+        <v-card tile class="ml-4 py-1 px-1 grey lighten-2">
+          <h1 class="teal darken-4 white--text text-center">Contact Info</h1>
+          <v-list-item v-for="(contact, i) in contacts" :key="i">
+                <v-list-item-icon>
+                  <v-icon v-text="contact.icon"></v-icon>
+                  <span class="ml-3"> {{contact.text}} </span>
+                </v-list-item-icon>
+          </v-list-item>
+        </v-card>
+      </v-col>
+      <v-col cols="7">
+        <v-card tile height="223px" class="mr-4 py-1 px-1 grey lighten-2">
+          <h1 class="teal darken-4 white--text text-center">Diplomas</h1>
+          <v-list-item v-for="(diploma, i) in diplomas" :key="i">
+            <v-list-item-content>
+              <v-list-item-title>
+                <v-list-item-icon>
+                  <v-icon v-text="diploma.icon"></v-icon>
+                  <span class="ml-3"> {{diploma.text}} </span>
+                </v-list-item-icon>
+              </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-app>
 </template>
 
 <script>
   export default {
-    name: 'home',
-    components: {
-
-    },
+    name: 'About',
     data: () => ({
       contacts: [
-        {
-          icon: 'mdi-email',
-          text: 'jassojorgealberto@gmail.com',
-        },
-        {
-          icon: 'mdi-cellphone-iphone',
-          text: '477 631 84 31',
-        },
-        {
-          icon: 'mdi-map-marker',
-          text: 'León GTO México',
-        },
+        { icon: 'mdi-email', text: 'jassojorgealberto@gmail.com' },
+        { icon: 'mdi-cellphone-iphone', text: '477 631 84 31' },
+        { icon: 'mdi-map-marker', text: 'León GTO México' }
       ],
       diplomas: [
         {
           icon: 'mdi-code-tags',
-          text: 'Cédula profesional de Técnico en Programador de Software',
+          text: 'Cédula profesional de Técnico Programador de Software'
         },
         {
           icon: 'mdi-certificate',
-          text: 'Acreditación del nivel intermedio del idioma inglés',
-        },
-      ],
-    }),
-  }
+          text: 'Acreditación del nivel intermedio del idioma inglés'
+        }
+      ]
+    })
+  };
 </script>
